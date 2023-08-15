3e7915d3e6b53831745779d3de75c6932dc14bec
SirLucian <lucian.blankevoort@gmail.com>
2023-08-16 01:13:32 +0200

    Refactor ImageGallery component to fetch images from Supabase and display them with infinite scroll functionality.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,25 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import ImageModal from '../components/ImageModal';
+import React, { useState, useEffect, useCallback } from 'react';
+import { supabase } from '@/utils/supabaseClient';
+import ImageModal from '@/components/ImageModal';
 import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 
 interface Image {
   id: string;
   url: string;
 }
-
-export default function ImageGallery ({ images }: Image[]) {
-    const [displayImages, setDisplayImages] = useState(3);
-
-    const [selectedImage, setSelectedImage] = useState<Image | null>(null);
+export default function ImageGallery () {
+  const [images, setImages] = useState([]);
+  const [displayImages, setDisplayImages] = useState(0);
+  const [selectedImage, setSelectedImage] = useState<Image | null>(null);
     
-    useInfiniteScroll({
-      trackElement: '#images-bottom',
-      containerElement: '#main'
-    }, () => {
-      setDisplayImages(oldVal => oldVal + 3);
-    });
-  
+  const fetchImages = useCallback(async () => {
+    let { data: stickers_view, error } = await supabase
+      .from('stickers_view')
+      .select('*')
+      .range(displayImages, displayImages + 9);
+
+        if (error) console.error(error);
+        else setImages([...images, ...stickers_view]);
+    }, [displayImages]);
+
+useEffect(() => {
+    fetchImages();
+}, [displayImages]);
+
+useInfiniteScroll({
+    trackElement: '#images-bottom',
+    containerElement: '#main'
+}, () => {
+    setDisplayImages(oldVal => oldVal + 10);
+});  
     const openModal = (image: Image) => {
       setSelectedImage(image);
     };
@@ -34,15 +46,15 @@ export default function ImageGallery ({ images }: Image[]) {
           <div className="container">
             <h1 style={{ textAlign: 'center' }}>Image Gallery</h1>
             {
-            images?.slice(0, displayImages).map((image: Image) => (
-              <img
-                key={image.id}
-                src={image.url}
-                alt=""
-                onClick={() => openModal(image)}
-              />
-            ))
-          }
+              images?.slice(0, displayImages).map((image: Image) => (
+                <img
+                  key={image.id}
+                  src={image.generated_images[0].url}
+                  alt=""
+                  onClick={() => openModal(image)}
+                />
+              ))
+              }
             <div id="images-bottom"></div>
           </div>
         </main>
