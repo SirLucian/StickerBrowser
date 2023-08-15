9c13eaad20a42c8e2b910d057f2a6afbca7e4bd4
SirLucian <lucian.blankevoort@gmail.com>
2023-08-15 23:58:37 +0200

    createNextApp

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import ImageGallery from 'components/ImageGallery'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className="App">
+      <ImageGallery />
+    </div>
+  )
+}
