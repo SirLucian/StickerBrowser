68f750d2040cce0578ad272a1ff88eadacd89ef8
SirLucian <lucian.blankevoort@gmail.com>
2023-08-16 01:04:11 +0200

    Refactored ImageGallery component to use infinite scroll and added server-side rendering for fetching images.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,85 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import ImageGallery from '@/components/ImageGallery'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ImageModal from '../components/ImageModal';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <div className="App">
-      <ImageGallery />
-    </div>
-  )
+interface Image {
+  id: string;
+  url: string;
 }
+
+export default function ImageGallery ({ images }: Image[]) {
+    const [displayImages, setDisplayImages] = useState(3);
+    // const [images, setImages] = useState<Image[]>([]);
+    const [hasMore, setHasMore] = useState(true);
+    const [selectedImage, setSelectedImage] = useState<Image | null>(null);
+    
+    useInfiniteScroll({
+      trackElement: '#images-bottom',
+      containerElement: '#main'
+    }, () => {
+      setDisplayImages(oldVal => oldVal + 3);
+    });
+  
+    const openModal = (image: Image) => {
+      setSelectedImage(image);
+    };
+  
+    const closeModal = () => {
+      setSelectedImage(null);
+    };
+    // useEffect(() => {
+    //     fetchImages();
+    //   }, []);
+    
+    //   const fetchImages = async () => {
+    //     const response = await axios.get<Image[]>('http://localhost:5000/images', {
+    //       params: { start: images.length, end: images.length + 50 },
+    //     });
+    //     setImages(images.concat(response.data));
+    //     if (response.data.length < 50) setHasMore(false);
+    //   };
+    
+    return (
+      <>
+        <main id="main">
+          <div className="container">
+            <h1 style={{ textAlign: 'center' }}>Image Gallery</h1>
+            {
+            images?.slice(0, displayImages).map((image: Image) => (
+              <img
+                key={image.id}
+                src={image.url}
+                alt=""
+                onClick={() => openModal(image)}
+              />
+            ))
+          }
+            <div id="images-bottom"></div>
+          </div>
+        </main>
+        {selectedImage && (
+          <ImageModal image={selectedImage} onClose={closeModal} />
+        )}
+      </>
+    )
+  }
+
+
+export async function getServerSideProps() {
+    let images:Image[] = [];
+  
+    try {
+    const res = await axios.get<Image[]>('http://localhost:5000/images');
+      images = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  
+    return {
+      props: {
+        images
+      },
+      revalidate: 10
+    }
+  }
