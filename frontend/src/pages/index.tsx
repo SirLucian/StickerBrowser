16e1cfdebeffbdaa88cbad7d4197dd9c4de4801e
SirLucian <lucian.blankevoort@gmail.com>
2023-08-18 00:48:31 +0200

    (no commit message provided)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,66 +1,19 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { supabase } from '@/utils/supabaseClient';
-import ImageModal from '@/components/ImageModal';
-import useInfiniteScroll from '@/hooks/useInfiniteScroll';
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import ImageGallery from '@/components/ImageGallery'
 
-interface Image {
-  id: string;
-  url: string;
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className="App">
+      <nav className='sticky top-0 h-16 w-full bg-slate-800 z-50 flex align-middle justify-between'>
+        <div className='container mx-auto align-middle justify-between text-lg'>
+          Stickems
+        </div>
+        
+      </nav>
+      <ImageGallery />
+    </div>
+  )
 }
-export default function ImageGallery () {
-  const [images, setImages] = useState([]);
-  const [displayImages, setDisplayImages] = useState(0);
-  const [selectedImage, setSelectedImage] = useState<Image | null>(null);
-    
-  const fetchImages = useCallback(async () => {
-    let { data: stickers_view, error } = await supabase
-      .from('stickers_view')
-      .select('*')
-      .range(displayImages, displayImages + 9);
-
-        if (error) console.error(error);
-        else setImages([...images, ...stickers_view]);
-    }, [displayImages]);
-
-useEffect(() => {
-    fetchImages();
-}, [displayImages]);
-
-useInfiniteScroll({
-    trackElement: '#images-bottom',
-    containerElement: '#main'
-}, () => {
-    setDisplayImages(oldVal => oldVal + 10);
-});  
-    const openModal = (image: Image) => {
-      setSelectedImage(image);
-    };
-  
-    const closeModal = () => {
-      setSelectedImage(null);
-    };
-    
-    return (
-      <>
-        <main id="main">
-          <div className="container">
-            <h1 style={{ textAlign: 'center' }}>Image Gallery</h1>
-            {
-              images?.slice(0, displayImages).map((image: Image) => (
-                <img
-                  key={image.id}
-                  src={image.generated_images[0].url}
-                  alt=""
-                  onClick={() => openModal(image)}
-                />
-              ))
-              }
-            <div id="images-bottom"></div>
-          </div>
-        </main>
-        {selectedImage && (
-          <ImageModal image={selectedImage} onClose={closeModal} />
-        )}
-      </>
-    )
-  }
