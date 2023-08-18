e63d388a659765768f3884fab9bc5b17051c0254
SirLucian <lucian.blankevoort@gmail.com>
2023-08-18 21:15:49 +0200

    aider: Added frontend\src\components\Navbar.tsx

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+      <nav className="sticky top-0 h-16 w-full bg-slate-800 z-50 flex align-middle justify-between">
+        <div className="container mx-auto align-middle justify-between text-lg">
+          Stickems
+        </div>
+      </nav>
+    );
+  }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,16 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import ImageGallery from '@/components/ImageGallery'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import ImageGallery from "@/components/ImageGallery";
+import Navbar from "@/components/Navbar";
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
     <div className="App">
-      <nav className='sticky top-0 h-16 w-full bg-slate-800 z-50 flex align-middle justify-between'>
-        <div className='container mx-auto align-middle justify-between text-lg'>
-          Stickems
-        </div>
-        
-      </nav>
+      <Navbar/>
       <ImageGallery />
     </div>
-  )
+  );
 }
